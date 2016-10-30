5b7e95b169fec05d700f484c7c22fdb7abf1e04c
FoysalOsmany <foysalo@gmail.com>
2016-10-30 21:14:36 +0600

    Refactor install script for Notifier

--- plugins/notifier/install.js
+++ plugins/notifier/install.js
@@ -1,6 +1,9 @@
-var async = require('async'),
-  pluginManager = require('../pluginManager.js'),
-  countlyDb = pluginManager.dbConnection();
+'use strict';
+
+const async = require('async');
+const Q = require('q');
+const pluginManager = require('../pluginManager.js');
+const countlyDb = pluginManager.dbConnection();
 
 console.log("Installing notifier plugin");
 countlyDb.collection('apps').find({}).toArray(function (err, apps) {
@@ -8,22 +11,17 @@ countlyDb.collection('apps').find({}).toArray(function (err, apps) {
   if (!apps || err) {
     return;
   }
-  function upgrade(app, done){
-    var cnt = 0;
+  function upgrade(app, done) {
     console.log("Creating notifier collection for " + app.name);
-    function cb(){
+    function cb() {
       done();
     }
-    countlyDb.command({"convertToCapped": 'notifier' + app._id, size: 1000000, max: 1000}, function(err,data){
-      if(err){
-        countlyDb.createCollection('notifier' + app._id, {capped: true, size: 1000000, max: 1000}, cb);
-      }
-      else{
-        cb();
-      }
-    });
+
+    Q.ninvoke(countlyDb, 'createCollection', `notifier_daily${app._id}`, cb);
+    Q.ninvoke(countlyDb.collection('app_users' + app._id), 'ensureIndex', {"date": 1, "condition": 1}, cb);
   }
-  async.forEach(apps, upgrade, function(){
+
+  async.forEach(apps, upgrade, function () {
     console.log("Notifier plugin installation finished");
     countlyDb.close();
   });
