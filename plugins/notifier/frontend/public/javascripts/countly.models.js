e03f4ba8555ddf921ac019a35449be4a9f92848c
FoysalOsmany <foysalo@gmail.com>
2016-10-30 21:12:46 +0600

    Implement countly.model for api call from frontend

--- plugins/notifier/frontend/public/javascripts/countly.models.js
+++ plugins/notifier/frontend/public/javascripts/countly.models.js
@@ -1,1 +1,24 @@
-CountlyHelpers.createMetricModel(window.countlyNotifier = window.countlyNotifier || {}, {name: "notifier", estOverrideMetric: "notifier"}, jQuery);
+(function (countlyNotifier, $) {
+
+  //Private Properties
+  var _data = [];
+
+  //Public Methods
+  countlyNotifier.initialize = function () {
+    return $.ajax({
+      type:"GET",
+      url:countlyCommon.API_URL + "/notifications/daily",
+      data:{
+        api_key: countlyGlobal['member'].api_key,
+        app_id: countlyCommon.ACTIVE_APP_ID
+      },
+      success:function (response) {
+        _data = response;
+      }
+    });
+  };
+
+  countlyNotifier.getData = function () {
+    return _data;
+  };
+}(window.countlyNotifier = window.countlyNotifier || {}, jQuery));
